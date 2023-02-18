1783364ccc296f4b04f46473745b75ae531ae221
LorenCuadero <105354289+LorenCuadero@users.noreply.github.com>
2023-02-18 15:20:46 +0800

    Factory Codes

---
+++ FormatFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Format>
+ */
+class FormatFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => fake()->title(),
+            'content' => fake()->title(),
+            'spot' => fake()->spot(),
+            'author' => fake()->author(),
+            // 'user_id' => User::first()
+
+        ];
+
+    }
+}
